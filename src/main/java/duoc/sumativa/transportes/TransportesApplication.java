8c13472812aee0b41f1d8b1e2403f11b73107a6d
Starcrusher037 <leo.bustamante@duocuc.cl>
2026-07-06 22:20:18 -0400

    asas

--- src/main/java/duoc/sumativa/transportes/TransportesApplication.java
+++ src/main/java/duoc/sumativa/transportes/TransportesApplication.java
@@ -10,5 +10,5 @@ public class TransportesApplication {
 		SpringApplication.run(TransportesApplication.class, args);
 	}
 
-	//pruebas de conecciona apiwateway, vamos con mente frias
+	//pruebas de conecciona apiwateway
 }
